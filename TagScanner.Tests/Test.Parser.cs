8e68aee438d85d2728ff5c1385466564a79a47b4
John Kerr <john@jmkerr.com>
2024-03-26 17:37:26 +0000

    parser unit tests (start)

---
+++ TagScanner.Tests/Test.Parser.cs
@@ -0,0 +1,18 @@
+﻿namespace TagScanner.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Linq;
+    using TagScanner.Terms;
+
+    public partial class Test
+    {
+        [TestMethod]
+        [DataRow("Compare(Album, \"Sgt Pepper\")", "Compare", "(", "Album", ",", "\"Sgt Pepper\"", ")")]
+        public void TestParseFunctions(string text, params string[] expected)
+        {
+            var parser = new Parser(text);
+            var actual = parser.AllTokens();
+            Assert.IsTrue(actual.SequenceEqual(expected));
+        }
+    }
+}
