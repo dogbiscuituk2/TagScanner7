08b9a1e054016e19a78b2f23ba02175bdf7d36e2
John Kerr <john@jmkerr.com>
2024-03-26 17:19:58 +0000

    add regex to parser

--- TagScanner/Terms/Parser.cs
+++ TagScanner/Terms/Parser.cs
@@ -3,10 +3,13 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Text.RegularExpressions;
     using Utils;
 
     public class Parser
     {
+        #region Constructor
+
         public Parser(string text)
         {
             Text = text;
@@ -14,18 +17,35 @@
             Index = 0;
         }
 
+        #endregion
+
+        #region Public Properties
+
+        public IEnumerable<string> AllTokens()
+        {
+            string token = null;
+            while(token != string.Empty)
+            {
+                token = PeekToken();
+                ReadPast(token);
+                yield return token;
+            }
+            yield break;
+        }
+
+        #endregion
+
+        #region Public Properties
+
         public string Text { get; private set; }
         public int Count { get; private set; }
         public int Index { get; private set; }
 
+        #endregion
+
+        #region Private Fields
+
         private const char Eof = (char)26;
-
-        #region Private Methods
-
-        private static IEnumerable<string> Functions => Methods.Keys;
-        private static IEnumerable<string> Tags => Terms.Tags.Keys.Select(p => p.DisplayName());
-        private static IEnumerable<string> Types => Cast.Types.Select(p => p.Say());
-        private static IEnumerable<string> Symbols => new[] { "(", ")", "?", ":", "&", "|", "^", "=", "!=", "<", ">=",">", "<=", "+", "-", "*", "/", "!", ",", "." };
 
         /// <summary>
         /// Names are ordered descending to that, for example, "Album Artists" is matched before "Artists".
@@ -33,16 +53,35 @@
         /// </summary>
         private static readonly string[] Tokens = Functions.Union(Tags).Union(Types).Union(Symbols).OrderByDescending(p => p).ToArray();
 
+        #endregion
+
+        #region Private Properties
+
+        private static IEnumerable<string> Functions => Methods.Keys;
+        private static IEnumerable<string> Tags => Terms.Tags.Keys.Select(p => p.DisplayName());
+        private static IEnumerable<string> Types => Cast.Types.Select(p => p.Say());
+        private static IEnumerable<string> Symbols => new[] { "(", ")", "?", ":", "&", "|", "^", "=", "!=", "<", ">=", ">", "<=", "+", "-", "*", "/", "!", ",", "." };
+
+        #endregion
+
+        #region Private Methods
+
+        private string MatchCharacter() => MatchRegex(@"^'([^\]|\.)'");
+        private string MatchNumber() => MatchRegex(@"^(\d+\.?\d*|)");
+        private string MatchRegex(string pattern) => Regex.Match(Text.Substring(Index), pattern).Value;
+        private string MatchString() => MatchRegex(@"^""([^\]|\.)*""");
+
         private string MatchToken()
         {
             var text = Text.Substring(Index);
             try { return Tokens.First(p => text.StartsWith(p)); }
-            catch (InvalidOperationException) { throw new FormatException($"Unrecognised name: '{text}'."); }
+            catch (InvalidOperationException) { throw new FormatException($"Unrecognised token: '{text}'."); }
         }
 
         private char PeekChar()
         {
-            while (Index < Count && Text[Index] == ' ')
+            // Skip whitespace.
+            while (Index < Count && Text[Index] <= ' ')
                 Index++;
             return Index < Count ? Text[Index] : Eof;
         }
@@ -55,10 +94,20 @@
             var nextChar = PeekChar();
             switch (nextChar)
             {
-
+                case '\'':
+                    return MatchCharacter();
+                case '"':
+                    return MatchString();
+                case char c when char.IsDigit(c):
+                    return MatchNumber();
+                case Eof:
+                    return string.Empty;
             }
-            throw new FormatException($"Unexpected character '{nextChar}' in input '{Text}' at position {Index}.");
+            UnexpectedCharacter(nextChar);
+            return string.Empty;
         }
+
+        private void UnexpectedCharacter(char c) => throw new FormatException($"Unexpected character '{c}' in input '{Text}' at position {Index}.");
 
         private void ReadPast(string token) => Index += token.Length;
 
