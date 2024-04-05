dee9a35ab78ac200966f06a5edc622d4833c85fa
DogBiscuitUK2 <johnmkerr@gmail.com>
2024-04-05 18:20:57 +0100

    perfect parser debug output & state class

--- TagScanner/Terms/ParserState.cs
+++ TagScanner/Terms/ParserState.cs
@@ -15,18 +15,18 @@
         public bool AnyTerms() => _terms.Any();
         public bool AnyTokens() => _tokens.Any();
 
-        public void BeginParse(string caller, int line, string text) => Process(caller, line, p => Reset(text), null, Options.Heading);
-        public Term EndParse(string caller, int line, Term term) { Process(caller, line, p => null); return term; }
-        public Term NewTerm(string caller, int line, Term term) { Process(caller, line, p => term, Options.Heading); return term; }
+        public void BeginParse(string caller, int line, string text) => Process(caller, line, p => Reset(text), Options.All);
         public Token DequeueToken(string caller, int line) => (Token)Process(caller, line, p => _tokens.Dequeue());
-        public void EnqueueToken(string caller, int line, Token token) => Process(caller, line, p => { _tokens.Enqueue((Token)p); return p; });
+        public Term EndParse(string caller, int line, Term term) { Process(caller, line, p => term); return term; }
+        public void EnqueueToken(string caller, int line, Token token) => Process(caller, line, p => { _tokens.Enqueue(token); return token; });
+        public Term NewTerm(string caller, int line, Term term) { Process(caller, line, p => term, 0); return term; }
         public Op PeekOperator(string caller, int line) => (Op)Process(caller, line, p => _operators.Peek(), 0);
         public Term PeekTerm(string caller, int line) => (Term)Process(caller, line, p => _terms.Peek(), 0);
         public Token PeekToken(string caller, int line) => (Token)Process(caller, line, p => _tokens.Peek(), 0);
         public Op PopOperator(string caller, int line) => (Op)Process(caller, line, p => _operators.Pop());
         public Term PopTerm(string caller, int line) => (Term)Process(caller, line, p => _terms.Pop());
-        public void PushOperator(string caller, int line, Op op) => Process(caller, line, p => { _operators.Push((Op)p); return p; });
-        public void PushTerm(string caller, int line, Term term) => Process(caller, line, p => { _terms.Push((Term)p); return p; });
+        public void PushOperator(string caller, int line, Op op) => Process(caller, line, p => { _operators.Push(op); return op; });
+        public void PushTerm(string caller, int line, Term term) => Process(caller, line, p => { _terms.Push(term); return term; });
 
         public override string ToString() => string.Format("{0,38}  {1}\r\n{2,38}  {3}\r\n{4,38}  {5}",
             "Tokens", Say(_tokens.Select(p => p.Value)),
@@ -49,14 +49,14 @@
         {
 #if DEBUG_PARSER
             const string format = "{0,17}{1,6}  {2,12}  {3}";
-            if (options == Options.Heading)
+            if ((options & Options.Heading) != 0)
             {
                 DrawRuler();
                 Debug.WriteLine(format, "Caller", "Line", "Action", "Value");
                 DrawRuler();
             }
             Debug.WriteLine(format, caller, line, action, value);
-            if (options == Options.State)
+            if ((options & Options.State) != 0)
             {
                 Debug.WriteLine(format, "", "", "Tokens", Say(_tokens.Select(p => p.Value)));
                 Debug.WriteLine(format, "", "", "Terms", Say(_terms));
@@ -71,8 +71,8 @@
         private void Exception(string caller, int line, Exception exception, [CallerMemberName] string action = "") =>
             Dump(caller, line, exception.GetAllInformation(), Options.State, action);
 
-        private object Process(string caller, int line, Func<object, object> process, object value = null,
-            Options options = Options.State, [CallerMemberName] string action = "")
+        private object Process(string caller, int line, Func<object, object> process, Options options = Options.State, object value = null,
+            [CallerMemberName] string action = "")
         {
             try
             {
@@ -103,7 +103,7 @@
 
         #region Private Types
 
-        private enum Options { None, State, Heading }
+        [Flags] private enum Options { None, State, Heading, All }
 
         #endregion
     }
