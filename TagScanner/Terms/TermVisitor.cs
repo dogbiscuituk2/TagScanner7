3887738dbe1e70391c578a40fc5d1bb995287db3
DogBiscuitUK2 <johnmkerr@gmail.com>
2024-04-11 08:27:55 +0100

    Make method IgnoreCase idempotent.

--- TagScanner/Terms/TermVisitor.cs
+++ TagScanner/Terms/TermVisitor.cs
@@ -5,7 +5,7 @@
     public static class TermVisitor
     {
         public static Term IgnoreCase(this Term term) => Visit(term, p =>
-            p.ResultType == typeof(string)
+            p.ResultType == typeof(string) && !(p is Function f && f.Fn == Fn.Uppercase)
                 ? new Function(Fn.Uppercase, p)
                 : p);
 
@@ -15,7 +15,7 @@
 
             Term DoVisit(Term t)
             {
-                if (t is TermList termList)
+                if (t is TermList termList) // Depth first.
                 {
                     var terms = termList.Operands;
                     for (var index = 0; index < terms.Count; index++)
