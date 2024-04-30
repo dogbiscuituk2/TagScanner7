7afe29eb1413a90d54516e8a66b4adcfff9c1a1c
John Kerr <john@jmkerr.com>
2024-04-30 13:46:40 +0100

    oops

--- TagScanner/Terms/OpInfo.cs
+++ TagScanner/Terms/OpInfo.cs
@@ -6,16 +6,17 @@
 
     public class OpInfo
     {
-        public OpInfo(char label, string format, ExpressionType expressionType, Rank rank, Type paramType)
-            : this(label, format, expressionType, rank, paramType, null, null) { }
+        #region Constructors
 
-        public OpInfo(char label, string format, ExpressionType expressionType, Rank rank, Type paramType, Type resultType)
-            : this(label, format, expressionType, rank, paramType, resultType, null) { }
+        public OpInfo(char label) : this(label, null, 0, 0, null, null) { }
+
+        public OpInfo(char label, string format, ExpressionType expressionType, Rank rank, Type paramType)
+            : this(label, format, expressionType, rank, paramType, null) { }
+
+        public OpInfo(char label, string format, ExpressionType expressionType, Rank rank, Image image)
+            : this(label, format, expressionType, rank, null, image) { }
 
         public OpInfo(char label, string format, ExpressionType expressionType, Rank rank, Type paramType, Image image)
-            : this(label, format, expressionType, rank, paramType, null, image) { }
-
-        public OpInfo(char label, string format, ExpressionType expressionType, Rank rank, Type paramType, Type resultType, Image image)
         {
             ExpressionType = expressionType;
             Format = format;
@@ -23,8 +24,11 @@
             Label = label.ToString();
             ParamType = paramType;
             Rank = rank;
-            //ResultType = resultType ?? paramType;
         }
+
+        #endregion
+
+        #region Fields
 
         public ExpressionType ExpressionType;
         public string Format;
@@ -34,6 +38,7 @@
         public bool ParamArray;
         public Type ParamType;
         public Rank Rank;
-        //public Type ResultType;
+
+        #endregion
     }
 }
