954e094a3104fb9892ba8f9c5c43e2adef6c66fa
John Kerr <john@jmkerr.com>
2024-05-06 14:31:13 +0100

    Add FCTB

---
+++ TagScanner/Controllers/ScriptFormController.cs
@@ -0,0 +1,52 @@
+﻿namespace TagScanner.Controllers
+{
+    using System.Drawing;
+    using System.Linq;
+    using System.Windows.Forms;
+    using FastColoredTextBoxNS;
+    using TagScanner.Terms;
+    using Views;
+
+    public class ScriptFormController : Controller
+    {
+        public ScriptFormController(Controller parent) : base(parent) { }
+
+        public ScriptForm ScriptForm => _scriptForm ?? CreateScriptForm();
+
+        public bool Execute()
+        {
+            var result = ScriptForm.ShowDialog(Owner) == DialogResult.OK;
+            return result;
+        }
+
+        private ScriptForm _scriptForm;
+
+        private FastColoredTextBox ColourTextBox => ScriptForm.ColourTextBox;
+
+        private ScriptForm CreateScriptForm()
+        {
+            _scriptForm = new ScriptForm();
+            ColourTextBox.Language = Language.Custom;
+            ColourTextBox.TextChanged += ColourTextBox_TextChanged;
+            return _scriptForm;
+        }
+
+        private static FontStyle style => FontStyle.Regular;
+        private static TextStyle
+            blue = new TextStyle(Brushes.LightBlue, null, style),
+            red = new TextStyle(Brushes.Red, null, style),
+            green = new TextStyle(Brushes.Green, null, style);
+
+        private string
+            functors = Functors.Keys.Select(p => $"{p}").Aggregate((p, q) => $"{p}|{q}"),
+            operators = Operators.Keys.Select(p => $"{p}").Aggregate((p, q) => $"{p}|{q}");
+
+        private void ColourTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            e.ChangedRange.ClearStyle(blue, red, green);
+            e.ChangedRange.SetStyle(blue, functors);
+            e.ChangedRange.SetStyle(red, operators);
+            e.ChangedRange.SetStyle(green, "{{! .*? }}");
+        }
+    }
+}
---
+++ TagScanner/Views/ScriptForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace TagScanner.Views
+{
+    partial class ScriptForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ScriptForm));
+            this.ColourTextBox = new FastColoredTextBoxNS.FastColoredTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ColourTextBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ColourTextBox
+            // 
+            this.ColourTextBox.AutoCompleteBracketsList = new char[] {
+        '(',
+        ')',
+        '{',
+        '}',
+        '[',
+        ']',
+        '\"',
+        '\"',
+        '\'',
+        '\''};
+            this.ColourTextBox.AutoScrollMinSize = new System.Drawing.Size(27, 14);
+            this.ColourTextBox.BackBrush = null;
+            this.ColourTextBox.CharHeight = 14;
+            this.ColourTextBox.CharWidth = 8;
+            this.ColourTextBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.ColourTextBox.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.ColourTextBox.Font = new System.Drawing.Font("Courier New", 9.75F);
+            this.ColourTextBox.IsReplaceMode = false;
+            this.ColourTextBox.Location = new System.Drawing.Point(12, 12);
+            this.ColourTextBox.Name = "ColourTextBox";
+            this.ColourTextBox.Paddings = new System.Windows.Forms.Padding(0);
+            this.ColourTextBox.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.ColourTextBox.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("ColourTextBox.ServiceColors")));
+            this.ColourTextBox.Size = new System.Drawing.Size(600, 231);
+            this.ColourTextBox.TabIndex = 0;
+            this.ColourTextBox.Zoom = 100;
+            // 
+            // ScriptForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ColourTextBox);
+            this.Name = "ScriptForm";
+            this.Text = "Script";
+            ((System.ComponentModel.ISupportInitialize)(this.ColourTextBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public FastColoredTextBoxNS.FastColoredTextBox ColourTextBox;
+    }
+}
