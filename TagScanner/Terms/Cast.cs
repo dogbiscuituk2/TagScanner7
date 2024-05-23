b1757fd5de04ccaf7c738dbc572814394758c6ea
John Kerr <john@jmkerr.com>
2024-05-23 23:41:57 +0100

    string to numeric casts

--- TagScanner/Terms/Cast.cs
+++ TagScanner/Terms/Cast.cs
@@ -11,7 +11,23 @@
 
         public Type NewType { get; set; }
 
-        public override Expression Expression => Expression.Convert(FirstSubExpression, ResultType);
+        public override Expression Expression
+        {
+            get
+            {
+                Type
+                    sourceType = Operands[0].ResultType,
+                    targetType = ResultType;
+                if (sourceType == typeof(string))
+                {
+                    foreach (var type in new[] { typeof(int), typeof(double), typeof(long), typeof(uint), typeof(float), typeof(ulong), typeof(decimal) })
+                        if (targetType == type)
+                            return Expression.Call(type.GetMethod("Parse", new[] { sourceType }), FirstSubExpression);
+                }
+                return Expression.Convert(FirstSubExpression, targetType);
+            }
+        }
+
         public override bool IsInfinitary => false;
         public override Rank Rank => Rank.Unary;
         public override Type ResultType => NewType;
