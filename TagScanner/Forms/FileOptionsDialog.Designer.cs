1dbb8bd5753c9935aa75f1f4b90fc0e8668d28ff
DogBiscuitUK2 <johnmkerr@gmail.com>
2024-07-01 21:05:14 +0100

    xfer

--- TagScanner/Forms/FileOptionsDialog.Designer.cs
+++ TagScanner/Forms/FileOptionsDialog.Designer.cs
@@ -614,6 +614,7 @@
             this.Controls.Add(this.dtpCreatedMin);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4);
             this.MinimizeBox = false;
             this.MinimumSize = new System.Drawing.Size(700, 330);
