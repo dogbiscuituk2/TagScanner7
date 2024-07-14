9c94e1b57de089cf252714e8c7d6cdd2bc89f1c1
DogBiscuitUK2 <johnmkerr@gmail.com>
2024-07-14 20:25:53 +0100

    xfer

---
+++ TagScanner/Controllers/UpdateController.cs
@@ -0,0 +1,40 @@
+﻿namespace TagScanner.Controllers
+{
+    using System;
+
+    public class UpdateController
+    {
+        #region Constructor
+
+        public UpdateController(Action action) => _action = action;
+
+        #endregion
+
+        #region Public Methods
+
+        public void Pause() => _pauseCount++;
+
+        public void Resume()
+        {
+            if (--_pauseCount == 0 && _pending)
+                Run();
+        }
+
+        public void Run()
+        {
+            _pending = _pauseCount != 0;
+            if (!_pending)
+                _action();
+        }
+
+        #endregion
+
+        #region Private Fields
+
+        private Action _action;
+        private int _pauseCount;
+        bool _pending;
+
+        #endregion
+    }
+}
