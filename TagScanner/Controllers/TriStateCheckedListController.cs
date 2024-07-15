001fdf138c0e9f109fba69a524f262ff77b1d0cc
John Kerr <john@jmkerr.com>
2024-07-15 17:53:11 +0100

    xfer

--- TagScanner/Controllers/TriStateCheckedListController.cs
+++ TagScanner/Controllers/TriStateCheckedListController.cs
@@ -1,5 +1,6 @@
 ﻿namespace TagScanner.Controllers
 {
+    using System.Diagnostics;
     using System.Windows.Forms;
 
     public class TriStateCheckedListController : Controller
@@ -9,6 +10,7 @@
         public TriStateCheckedListController(Controller parent, CheckedListBox control) : base(parent)
         {
             _control = control;
+            _control.ItemCheck += Control_ItemCheck;
         }
 
         #endregion
@@ -16,6 +18,27 @@
         #region Private Fields
 
         private CheckedListBox _control;
+
+        #endregion
+
+        #region Event Handlers
+
+        private void Control_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            var newValue = NextState();
+            Debug.WriteLine(newValue);
+            e.NewValue = newValue;
+
+            CheckState NextState()
+            {
+                switch (e.CurrentValue)
+                {
+                    case CheckState.Indeterminate: return CheckState.Checked;
+                    case CheckState.Checked: return CheckState.Unchecked;
+                    default: return CheckState.Indeterminate;
+                }
+            }
+        }
 
         #endregion
     }
