d72c19c3271f7d1ae116bcf7cb2fae535917230e
John Kerr <john@jmkerr.com>
2024-07-18 13:01:45 +0100

    xfer

---
+++ TagScanner/Controls/ToolStripCheckBox.cs
@@ -0,0 +1,57 @@
+﻿namespace TagScanner.Controls
+{
+    using System;
+    using System.Windows.Forms;
+
+    public class ToolStripCheckBox : ToolStripControl<CheckBox>
+    {
+        #region Constructor
+
+        public ToolStripCheckBox() : base() { }
+
+        #endregion
+
+        #region Public Properties
+
+        public CheckState CheckState
+        {
+            get => Control.CheckState;
+            set => Control.CheckState = value;
+        }
+
+        public override string Text
+        {
+            get => Control.Text;
+            set => Control.Text = value;
+        }
+
+        #endregion
+
+        #region Public Events
+
+        public event EventHandler CheckStateChanged;
+
+        #endregion
+
+        #region Protected Methods
+
+        protected override void OnSubscribeEvents(CheckBox checkBox)
+        {
+            checkBox.CheckStateChanged += OnCheckStateChanged;
+        }
+
+        protected override void OnUnsubscribeEvents(CheckBox checkBox)
+        {
+            checkBox.CheckStateChanged -= OnCheckStateChanged;
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        private void OnCheckStateChanged(object sender, EventArgs e) =>
+            CheckStateChanged?.Invoke(this, EventArgs.Empty);
+
+        #endregion
+    }
+}
---
+++ TagScanner/Controls/ToolStripControl.cs
@@ -0,0 +1,38 @@
+﻿namespace TagScanner.Controls
+{
+    using System.Windows.Forms;
+
+    public class ToolStripControl<T> : ToolStripControlHost where T : Control, new()
+    {
+        #region Constructor
+
+        public ToolStripControl() : base(new T()) { }
+
+        #endregion
+
+        #region Public Properties
+
+        public new T Control => base.Control as T;
+
+        #endregion
+
+        #region Protected Methods
+
+        protected override void OnSubscribeControlEvents(Control control)
+        {
+            base.OnSubscribeControlEvents(control);
+            OnSubscribeEvents((T)control);
+        }
+
+        protected override void OnUnsubscribeControlEvents(Control control)
+        {
+            base.OnUnsubscribeControlEvents(control);
+            OnUnsubscribeEvents((T)control);
+        }
+
+        protected virtual void OnSubscribeEvents(T control) { }
+        protected virtual void OnUnsubscribeEvents(T control ) { }
+
+        #endregion
+    }
+}
