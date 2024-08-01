944400ef3922a9c22d156f7179f5fa233b147847
DogBiscuitUK2 <johnmkerr@gmail.com>
2024-08-01 21:44:10 +0100

    xfer

---
+++ TagScanner/Models/TagxCollection.cs
@@ -0,0 +1,36 @@
+﻿namespace TagScanner.Models
+{
+    using System.Collections.Generic;
+
+    public class TagxCollection
+    {
+        #region Constructors
+
+        public TagxCollection() => _tags = new List<Tagx>();
+        public TagxCollection(IEnumerable<Tagx> tags) : this() => _tags.AddRange(tags);
+
+        #endregion
+
+        #region Public Properties
+
+        public IEnumerable<Tagx> Tags => GetTags();
+
+        #endregion
+
+        #region Private Fields
+
+        private List<Tagx> _tags;
+
+        #endregion
+
+        #region Private Methods
+
+        private IEnumerable<Tagx> GetTags()
+        {
+            foreach (var tag in _tags)
+                yield return tag;
+        }
+
+        #endregion
+    }
+}
--- TagScanner/Models/TagxData.cs
+++ TagScanner/Models/TagxData.cs
@@ -1,6 +1,5 @@
 ﻿namespace TagScanner.Models
 {
-    using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Windows.Forms;
